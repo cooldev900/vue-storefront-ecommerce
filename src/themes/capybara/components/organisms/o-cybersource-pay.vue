2df54622000e0feebe314a7f180984bebf5e7c0c
cooldev900 <cooldev900@gmail.com>
2022-06-24 15:04:32 +0000

    udpating for sha sign

--- src/themes/capybara/components/organisms/o-cybersource-pay.vue
+++ src/themes/capybara/components/organisms/o-cybersource-pay.vue
@@ -42,8 +42,8 @@
 import {
   SfButton
 } from '@storefront-ui/vue';
-import { getShaSignatureBase64 } from 'theme/helpers/index.ts';
 import { mapGetters } from 'vuex';
+import { currentStoreView } from '@vue-storefront/core/lib/multistore';
 
 export default {
   name: 'OCybersourcepay',
@@ -52,32 +52,47 @@ export default {
     // PaymentStripe
   },
   async mounted () {
-    const passPhrase = ',3p%LhsENFp44Wn@ycK';
-    // this.edpqForm.reference_number = this.cartToken;
-    const cartTotalPrice = this.prices.grand_total;
-    this.edpqForm.amount = this.prices.grand_total * 100;    
-    this.edpqForm.signed_date_time = new Date().toISOString().substring(0, 19) + 'Z';
-    this.edpqForm.transaction_uuid = Math.floor(Math.random() * 100000000);
+    this.makeform();
+  },
+  methods: {
+    makeform() {
+      const storeId = currentStoreView().storeId
 
-    this.edpqForm.bill_to_forename = this.getPersonalDetails.firstName;
-    this.edpqForm.bill_to_surname = this.getPersonalDetails.lastName;
-    this.edpqForm.bill_to_email = this.getPersonalDetails.emailAddress;
-    this.edpqForm.bill_to_address_line1 = this.getPaymentDetails.streetAddress;
-    this.edpqForm.bill_to_address_country = this.getPaymentDetails.country;
-    this.edpqForm.bill_to_address_city = this.getPaymentDetails.city;
-
-    let signedFieldNames = this.edpqForm.signed_field_names.split(",");
-    let data = [];
-    let edpqForm = this.edpqForm;
-    signedFieldNames.forEach(function (item) {
-        console.log(item, 'key');
-        data.push(item + "=" + edpqForm[item]);
-    });
-    data = data?.length ? data.join(",") : "";
-    const secretKey = "9f8f91900c274d37925c0be4beebed23b8bc48776e4f4f63a2aa1a0b001dea0140bcac7ae391456989e90ae5e37c6b18c6caca3b5ef64b55a6395a7d0cc0a8d7511c9dae3b8748859c1153206009ad4582e70787acd7488584e6dd7a6ce0f1a3834ade1543da40758044c4fc3849ca0fba812aeca1e0487ebdf04ae2749c1729";
-    console.log(data, 'signature');
-    this.edpqForm.signature = this.$CryptoJS.HmacSHA256(data, secretKey).toString(this.$CryptoJS.enc.Base64);
-    console.log(this.edpqForm, 'wdpaForm');
+      this.edpqForm.access_key = 'a0b2da90c5d433f6892066d11b97e059';
+      this.edpqForm.currency = "QAR";
+      this.edpqForm.locale = storeId === 3 ? "ar-xn" : "en-US";
+      this.edpqForm.profile_id = '95937600-7A9B-425E-A47C-4F012E77487F';
+      this.edpqForm.signed_field_names = "access_key,amount,currency,locale,profile_id,reference_number,signed_date_time,signed_field_names,transaction_type,transaction_uuid,bill_to_forename,bill_to_surname,bill_to_email,bill_to_address_line1,bill_to_address_country,bill_to_address_city";
+      this.edpqForm.transaction_type = "sale";
+      this.edpqForm.reference_number = this.cartToken;
+      this.edpqForm.amount = this.prices.grand_total * 100;    
+      this.edpqForm.signed_date_time = new Date().toISOString().substring(0, 19) + 'Z';
+      this.edpqForm.transaction_uuid = Math.floor(Math.random() * 100000000);
+      this.edpqForm.bill_to_forename = this.getPersonalDetails.firstName;
+      this.edpqForm.bill_to_surname = this.getPersonalDetails.lastName;
+      this.edpqForm.bill_to_email = this.getPersonalDetails.emailAddress;
+      this.edpqForm.bill_to_address_line1 = this.getPaymentDetails.streetAddress;
+      this.edpqForm.bill_to_address_country = this.getPaymentDetails.country;
+      this.edpqForm.bill_to_address_city = this.getPaymentDetails.city;
+      let signedFieldNames = this.edpqForm.signed_field_names.split(",");
+      let data = [];
+      let edpqForm = this.edpqForm;
+      signedFieldNames.forEach(function (item) {
+          console.log(item, 'key');
+          data.push(item + "=" + edpqForm[item]);
+      });
+      data = data?.length ? data.join(",") : "";
+      const secretKey = "9f8f91900c274d37925c0be4beebed23b8bc48776e4f4f63a2aa1a0b001dea0140bcac7ae391456989e90ae5e37c6b18c6caca3b5ef64b55a6395a7d0cc0a8d7511c9dae3b8748859c1153206009ad4582e70787acd7488584e6dd7a6ce0f1a3834ade1543da40758044c4fc3849ca0fba812aeca1e0487ebdf04ae2749c1729";
+      this.edpqForm.signature = this.$CryptoJS.HmacSHA256(data, secretKey).toString(this.$CryptoJS.enc.Base64);
+    },
+  },
+  watch: {
+    getPaymentDetails() {
+      this.makeform();
+    },
+    getPersonalDetails() {
+      this.makeform();
+    },
   },
   computed: {
     ...mapGetters({
@@ -98,15 +113,15 @@ export default {
       shaSign: '',
       edpqForm: {
         access_key: 'a0b2da90c5d433f6892066d11b97e059',
-        amount: 200,
+        amount: 0,
         currency: "QAR",
         locale: "en-US",
         profile_id: '95937600-7A9B-425E-A47C-4F012E77487F',
-        reference_number: "100",        
+        reference_number: "",        
         signed_date_time: "",
         signed_field_names: "access_key,amount,currency,locale,profile_id,reference_number,signed_date_time,signed_field_names,transaction_type,transaction_uuid,bill_to_forename,bill_to_surname,bill_to_email,bill_to_address_line1,bill_to_address_country,bill_to_address_city",
         transaction_type: "sale",
-        transaction_uuid: "fcfc212e92d23be881d1299ef3c3b314",             
+        transaction_uuid: "",             
         signature: "",
         bill_to_forename: "",
         bill_to_surname: "",
